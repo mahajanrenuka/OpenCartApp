22655af5c652663df35a376a092935e15c38ee24
Renuka  Mahajan <renukamahajan@renukas-air.lan>
2024-07-01 17:45:43 -0700

    added demo page

---
+++ src/main/java/com/qa/opencart/pages/DemoPage.java
@@ -0,0 +1,13 @@
+package com.qa.opencart.pages;
+
+import org.openqa.selenium.By;
+
+import io.opentelemetry.exporter.logging.SystemOutLogRecordExporter;
+
+public class DemoPage {
+	
+	By locator1 = By.id("Demo");
+	System.out.println(locator1)
+
+
+}
